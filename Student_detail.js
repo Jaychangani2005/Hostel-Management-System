71a3c11b1bc9e06b3a0686b82fa0e51fc8b24706
Atibali <107742861+Atibali@users.noreply.github.com>
2024-10-08 11:33:44 +0530

    Delete Student_detail.js

--- Student_detail.js
+++
@@ -1,5 +0,0 @@
-// You can use this file to fetch data dynamically if needed
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Student details page loaded");
-    // You can modify the content using JavaScript if necessary
-});
